7e793c5d442d7269939fb9b1d79779d723cf178e
Itachi <babuto@Itachis-MacBook-Air.local>
2026-01-24 22:55:00 +0530

    adding conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,31 @@
+import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def setup():
-    service=Service(ChromeDriverManager().install())
-    driver=webdriver.Chrome(service=service)
-    driver.maximize_window()
+    sauce_username = os.getenv("SAUCE_USERNAME")
+    sauce_access_key = os.getenv("SAUCE_ACCESS_KEY")
+
+    sauce_url = f"https://{sauce_username}:{sauce_access_key}@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
+
+    options = Options()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "latest")
+
+    sauce_options = {
+        "name": "Pytest Login Tests",
+        "build": "GitHub Actions Build",
+        "seleniumVersion": "4.18.1"
+    }
+
+    options.set_capability("sauce:options", sauce_options)
+
+    driver = webdriver.Remote(
+        command_executor=sauce_url,
+        options=options
+    )
+
     yield driver
     driver.quit()
