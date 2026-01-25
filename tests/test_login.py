fff24cdb4a083f5af49b3d97ed5c4053b2bdf367
Itachi <babuto@Itachis-MacBook-Air.local>
2026-01-25 22:02:54 +0530

    adding headless

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,20 +1,23 @@
-from pages.login_page import LoginPage
-from config.config import *
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 
-def test_valid_login(setup):
-    driver=setup
-    login_page=LoginPage(driver)
+def test_valid_login():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    login_page.open(BASE_URL)
-    login_page.login(VALID_USERNAME,VALID_PASSWORD)
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.saucedemo.com/")
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    time.sleep(2)
+
     assert "inventory" in driver.current_url
-    
-def test_invalid_login(setup):
-    driver=setup
-    login_page= LoginPage(driver)
 
-    login_page.open(BASE_URL)
-    login_page.login(INVALID_USERNAME,INVALID_PASSWORD)
-
-    error_msg=login_page.get_error_message()
-    assert "Epic sadface: Username and password do not match any user in this service" in error_msg
+    driver.quit()
