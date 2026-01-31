4454a6b7d9bb689ca9302ea1e38560aa2c2b70a3
thvbhavya <thvbhavya@gmail.com>
2026-01-31 19:10:56 +0530

    adding code

--- pages/add_to_cart.py
+++ pages/add_to_cart.py
@@ -1,15 +1,19 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class addTocart:
 
     PRODUCTS_BTN = (By.CSS_SELECTOR, "a[href='/products']")
     VIEWPRODUCT = (By.CSS_SELECTOR, "a[href='/product_details/1']")
-    ADDTOCARTBTN = (By.CSS_SELECTOR, "button[type='button']")
+    ADDTOCARTBTN = (By.CSS_SELECTOR,"button[type='button']")
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def add_to_cart(self):
-        self.driver.find_element(*self.PRODUCTS_BTN).click()
-        self.driver.find_element(*self.VIEWPRODUCT).click()
-        self.driver.find_element(*self.ADDTOCARTBTN).click()
+        self.wait.until(EC.element_to_be_clickable(self.PRODUCTS_BTN)).click()
+
+        self.wait.until(EC.element_to_be_clickable(self.VIEWPRODUCT)).click
+
+        self.wait.until(EC.element_to_be_clickable(self.ADDTOCARTBTN)).click()
